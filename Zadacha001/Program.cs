1f48a22fa8b8686b1d99b254fc4db6b2a62865ad
Ваше имя английскими буквами <Zhenka_512@mail.ru>
2023-02-16 16:15:42 +0300

    квадрат числа

---
+++ Zadacha001/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу,которая на вход принимает число и выдает его квадрат (число умноженное на число)
+// например:
+//4 -> 16
+//-3 -> 9
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number =int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} -> {sqr}({number * number})");
+
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} -> {sqr1}");
---
+++ Zadacha002/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу,которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+// Например:
+//a = 25; b = 5 -> да
+//a = 2; b = 5 -> нет
+Console.Clear();
+Console.Write("Введите число a: "); 
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: "); 
+int b = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"{Math.Pow(b, 2) == a}");
+if(Math.Pow(b, 2) == a)
+{
+    Console.WriteLine("a квадрат b");
+}
+else
+{
+    Console.WriteLine("a не квадрат b");
+}
